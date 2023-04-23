74c15cf88513ce966b2db656de78e7f51384a4de
cmarun5 <131541414+cmarun5@users.noreply.github.com>
2023-04-23 23:29:49 +0530

    week1day2

---
+++ Palindrome.java
@@ -0,0 +1,24 @@
+package week1day2assignment;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		int r, sum = 0, temp;
+		int m = 34343;
+		int n = m;
+		temp = n;
+		while (n > 0) {
+			r = n % 10;
+			sum = (sum * 10) + r;
+			n = n / 10;
+		}
+		if (temp == sum) {
+			System.out.println(m + " is a palindrome number.");
+		} else {
+			System.out.println(m + "is not a palindrome number.");
+		}
+	}
+
+}
+
+
