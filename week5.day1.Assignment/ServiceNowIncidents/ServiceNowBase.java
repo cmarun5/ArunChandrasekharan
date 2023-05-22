e19a505505dbb60f06f09950e25a744f57a5fd1e
cmarun5 <131541414+cmarun5@users.noreply.github.com>
2023-05-22 22:41:53 +0530

    week5.day1.Assignment

---
+++ week5.day1.Assignment/ServiceNowIncidents/ServiceNowBase.java
@@ -0,0 +1,40 @@
+package week5.day1.assignment;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.sukgu.Shadow;
+
+public class ServiceNowBase {
+		
+		public ChromeDriver driver;
+		@BeforeMethod	
+		public void preCondition() {
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://dev139543.service-now.com/");
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+			driver.findElement(By.xpath("//input[@id='user_name']")).sendKeys("admin");
+			driver.findElement(By.xpath("//input[@id='user_password']")).sendKeys("@Run$ekharan21");
+			driver.findElement(By.xpath("//button[@id='sysverb_login']")).click();
+			Shadow shadow = new Shadow(driver);
+			shadow.setImplicitWait(10);
+			WebElement all = shadow.findElementByXPath("//div[contains(@class,'sn-polaris-navigation polaris-header-menu')]/div[text()='All']");
+			Actions builder = new Actions (driver);
+			builder.moveToElement(all).perform();
+			Actions builder2=new Actions(driver);
+			builder2.click(all).perform();
+			shadow.findElementByXPath("//input[@id='filter']").sendKeys("Incidents");
+			shadow.findElementByXPath("//mark[text()='Incidents']").click();
+		}
+		@AfterMethod
+		public void postCondition() {
+			driver.close();
+	}
+}
